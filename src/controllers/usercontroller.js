ea62420e35f26ace7bdea25c78dabb9ce5b0e8cd
mintonofall <mintonofall@gmail.com>
2022-07-08 17:00:14 +0900

    errormassage 추가

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -4,23 +4,30 @@ export const getJoin = (req, res) => {
 }
 
 export const postJoin = async (req, res) => {
-    const { name, phoneNumber, gender, password, grade, age } = req.body;
-
+    const { name, phoneNumber, gender, password, password2, grade, age } = req.body;
+    const pageTitle = "Join";
     console.log(name, phoneNumber, password, grade, age);
+
+    if (password !== password2) {
+        res.status(400);
+        res.render("join", { pageTitle, errorMessage: "password not match"}); 
+    } else {
+        try {
+            await User.create({
+            name,
+            phoneNumber,
+            password,
+            gender,
+            grade,
+            age,
+            });
+            return res.redirect("/");
+        } catch (error) {
+            return res.status(400).send(error);
+            };
+
+    }
     
-    try {
-        await User.create({
-          name,
-          phoneNumber,
-          password,
-          gender,
-          grade,
-          age,
-        });
-        return res.redirect("/");
-    } catch (error) {
-        return res.status(400).send(error);
-        };
 }
 
 
